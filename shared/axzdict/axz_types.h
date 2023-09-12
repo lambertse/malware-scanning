81afc297ffbecf700242d4a54547b404571009a2
lambertse <lambert.software.engineer@gmail.com>
2023-09-12 18:07:36 +0700

    init

---
+++ shared/axzdict/axz_types.h
@@ -0,0 +1,13 @@
+#ifndef __axz_types__
+#define __axz_types__
+
+#include <vector>
+#include <string>
+#include "axz_native.h"
+
+using axz_wstring = std::wstring;
+using axz_string = std::string;
+using axz_bytes = std::basic_string<axz_byte>;
+using axz_wstring_vector = std::vector<axz_wstring>;
+
+#endif
